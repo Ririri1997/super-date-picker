387e2581eb996477eb6dadd4a1ec0ffc8d381197
Ririri1997 <ircher1997@gmail.com>
2025-06-27 17:43:41 +0300

    create handleChange + styles

--- src/components/SuperDatePicker/DateRangeSelector.tsx
+++ src/components/SuperDatePicker/DateRangeSelector.tsx
@@ -1,23 +1,52 @@
 import DatePicker from "react-datepicker";
-import styles from "./DateRangeSelector.module.css"
+import styles from "./DateRangeSelector.module.css";
 import "react-datepicker/dist/react-datepicker.css";
-import { DateRange } from "../../types/index.ts";
+import type { DateRange } from "../../types/index.ts";
 
 type Props = {
   range: DateRange;
   onChange: (range: DateRange) => void;
 };
 
-
 function DateRangeSelector({ range, onChange }: Props) {
+  const handleFromChange = (date: Date | null) => {
+    if (date) {
+      console.log(date);
+      onChange({ ...range, from: date });
+    }
+  };
+
+  const handleToChange = (date: Date | null) => {
+    if (date) {
+      console.log(date);
+      onChange({ ...range, to: date });
+    }
+  };
 
   return (
-    <div className={styles['date-wrapper']}>
-    <DatePicker/>
-
-
+    <div className={styles["wrapper"]}>
+      <div className={styles["input"]}>
+      <label htmlFor="from">From</label>
+      <DatePicker
+        selected={range.from}
+        onChange={handleFromChange}
+        showTimeSelect
+        dateFormat="Pp"
+        id="from"
+      />
+      </div>
+      <div className={styles["input"]}>
+      <label htmlFor="to">To</label>
+      <DatePicker
+        selected={range.to}
+        onChange={handleToChange}
+        showTimeSelect
+        dateFormat="Pp"
+        id="to"
+      />
+      </div>
     </div>
-  )
+  );
 }
 
-export default  DateRangeSelector;
+export default DateRangeSelector;
--- src/components/SuperDatePicker/SuperDatePicker.tsx
+++ src/components/SuperDatePicker/SuperDatePicker.tsx
@@ -1,9 +1,19 @@
-import DateRangeSelector from "./DateRangeSelector"
+import { useState } from "react";
+import DateRangeSelector from "./DateRangeSelector";
+import type { DateRange } from "../../types";
 
-function SuperDatePicker(){
-  return(
-    <DateRangeSelector/>
-  )
-} 
+function SuperDatePicker() {
+  const [range, setRange] = useState<DateRange>({
+    from: new Date(),
+    to: new Date(),
+  });
 
-export default SuperDatePicker
+  return (
+    <DateRangeSelector
+      range={range}
+      onChange={(newRange) => setRange(newRange)}
+    />
+  );
+}
+
+export default SuperDatePicker;
